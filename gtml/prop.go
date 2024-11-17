292eb517f82f4d2faf6c8b0a36ad8b45d41c3c34
Phillip England <phillip.mark.england@gmail.com>
2024-11-17 05:45:49 -0600

    good saving point, about to start tearing down prop handling

--- gtml/prop.go
+++ gtml/prop.go
@@ -19,18 +19,53 @@ func NewProp(str string) (Prop, error) {
 	if val == "" {
 		return nil, fmt.Errorf("empty prop tag provided: %s", str)
 	}
-	if strings.Count(val, ".") == 1 {
-		// forTypeProp,
-
+	if strings.Count(val, ".") == 1 && len(strings.Split(val, ".")) == 2 {
+		prop, err := NewPropForType(str, val)
+		if err != nil {
+			return nil, err
+		}
+		return prop, nil
 	}
 	return nil, nil
 }
 
 // ##==================================================================
-type ForTypeProp struct {
+type PropForType struct {
 	Raw   string
 	Value string
 }
+
+func NewPropForType(raw string, val string) (*PropForType, error) {
+	return nil, nil
+}
+
+// ##==================================================================
+type PropForStr struct {
+	Raw   string
+	Value string
+}
+
+func NewPropForStr(raw string, val string) (*PropForType, error) {
+	return nil, nil
+}
+
+// ##==================================================================
+type PropStr struct {
+	Raw   string
+	Value string
+}
+
+func NewPropStr(raw string, val string) (*PropForType, error) {
+	return nil, nil
+}
+
+// ##==================================================================
+
+// ##==================================================================
+
+// ##==================================================================
+
+// ##==================================================================
 
 // ##==================================================================
 
